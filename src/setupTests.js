a93927ee64ef2544e0d50f6c0c07b3c39d0f3d61
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-03-27 18:18:11 +0200

    setup testing environment

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,10 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+//import "@testing-library/jest-dom";
+
+/* This code tells Enzyme to use the adapter for React 17, after which, it will be ready to use for developing your project! */
+import Enzyme from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+Enzyme.configure({ adapter: new Adapter() });
