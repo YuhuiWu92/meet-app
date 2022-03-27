14f45545311a529f9a2718bf788ba75603998fe9
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-03-27 22:12:47 +0200

    Scenario 1: Show Upcoming Events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./App.css";
+import EventList from "./EventList";
+import "./App.css";
 
 export default function App() {
-  return <div className="App">abc</div>;
+  return (
+    <div className="App">
+      <EventList />
+    </div>
+  );
 }
