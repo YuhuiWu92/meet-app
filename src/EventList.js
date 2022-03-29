9f084d7b9bbdbb36675a858bf098f151953e46e9
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-03-29 22:20:01 +0200

    fix css

--- src/EventList.js
+++ src/EventList.js
@@ -8,7 +8,7 @@ export default class EventList extends Component {
     return (
       <ul className="EventList">
         {events.map((event) => (
-          <Col xs={12} md={6} lg={4} key={event.id}>
+          <Col xs={12} md={10} key={event.id}>
             <Event event={event} />
           </Col>
         ))}
