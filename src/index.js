e863e973fb190639714f8faf2e315832284a8a5f
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-03-31 21:46:52 +0200

    add atatus for task 4.6

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import * as atatus from "atatus-spa";
+atatus.config("60ebd5dc24fe450ca89fbbb48f105b28").install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +13,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
